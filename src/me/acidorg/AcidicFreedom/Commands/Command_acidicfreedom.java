30aa86d867f5ec802a9bc981be7e47371b7ddbc0
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-19 01:03:45 -0600

    Create Command_acidicfreedom.java

---
+++ src/me/acidorg/AcidicFreedom/Commands/Command_acidicfreedom.java
@@ -0,0 +1,17 @@
+package me.acidorg.AcidicFreedom.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
+public class Command_acidicfreedom extends AF_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+      sender.sendMessage(ChatColor.AQUA + "Hello " + sender.getName() + " this is AcidicFreedom 1.5! Made by xXWilee999Xx and AcidicCyanide.");
+    }
+
+}
