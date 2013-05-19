c895ae50d1f19a2bf817b430a3ffdc3c1f38ae86
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-18 23:36:40 -0600

    Update AcidicFreedom.java

--- src/me/acidorg/AcidicFreedom/AcidicFreedom.java
+++ src/me/acidorg/AcidicFreedom/AcidicFreedom.java
@@ -1,10 +1,21 @@
-//This is the best plugin ever.
+package me.acidorg.AcidicFreedom;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class AcidicFreedom extends JavaPlugin
 {
-  @Override
-  public void onDisable();
-  log.log
-  
-  @Override
-  public void onEnable();
-  log.log
+    private Logger log = Logger.getLogger("Minecraft");
+
+    public void onEnable()
+    {
+        log.info("AcidicFreedom has been enabled!");
+    }
+
+    public void onDisable()
+    {
+        log.info("AcidicFreedom has been disabled! :(");
+    }
+
+}
