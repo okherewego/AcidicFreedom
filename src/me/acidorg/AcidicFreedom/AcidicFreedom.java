1647e2fe615042e58cbc8d0a924df6484b9fad39
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-18 23:32:59 -0600

    Update AcidicFreedom.java

--- src/me/acidorg/AcidicFreedom/AcidicFreedom.java
+++ src/me/acidorg/AcidicFreedom/AcidicFreedom.java
@@ -1,1 +1,10 @@
-This is the best plugin ever.
+//This is the best plugin ever.
+public class AcidicFreedom extends JavaPlugin
+{
+  @Override
+  public void onDisable();
+  log.log
+  
+  @Override
+  public void onEnable();
+  log.log
