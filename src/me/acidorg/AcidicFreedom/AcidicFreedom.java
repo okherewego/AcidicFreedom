2739ca8612b73efce9dcb1a723d098a155d42e6b
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-18 23:38:27 -0600

    Update AcidicFreedom.java

--- src/me/acidorg/AcidicFreedom/AcidicFreedom.java
+++ src/me/acidorg/AcidicFreedom/AcidicFreedom.java
@@ -2,6 +2,9 @@ package me.acidorg.AcidicFreedom;
 
 import java.util.logging.Logger;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AcidicFreedom extends JavaPlugin
@@ -16,6 +19,13 @@ public class AcidicFreedom extends JavaPlugin
     public void onDisable()
     {
         log.info("AcidicFreedom has been disabled! :(");
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+        Player player = (Player) sender;
+    	
+		return false;
     }
 
 }
