d5813bebef52fb1f29e4f1a845d4d188ce3bf191
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-19 16:54:55 -0700

    Removed all files

--- src/me/acidorg/AcidicFreedom/Commands/AdminLevel.java
+++
@@ -1,6 +0,0 @@
-package me.acidorg.AcidicFreedom.Commands;
-
-public enum AdminLevel
-{
-    ALL, OP, SUPER, SENIOR
-}
--- src/me/acidorg/AcidicFreedom/Commands/SourceType.java
+++
@@ -1,7 +0,0 @@
-package me.acidorg.AcidicFreedom.Commands;
-
-public enum SourceType
-{
-   ONLY_IN_GAME, ONLY_CONSOLE, BOTH
-   
-}
